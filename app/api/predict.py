309ea982d4a25a7040f8e380ef23a7a584c272d1
JonRivera <jonatanalejandrorivera@gmail.com>
2020-08-25 16:54:14 -0700

    updated base model code

--- app/api/predict.py
+++ app/api/predict.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASILICA_KEY = os.getenv('BASILICA_KEY')
-#BASILICA_KEY = "79a607aa-3e02-eb67-96cb-a7b21ff06e79"
+BASILICA_KEY = "79a607aa-3e02-eb67-96cb-a7b21ff06e79"
 log = logging.getLogger(__name__)
 router = APIRouter()
 
@@ -31,7 +31,7 @@ BASILICA = basilica.Connection(BASILICA_KEY)
 
 # update
 
-class Item(BaseModel):
+class Subreddit_Post(BaseModel):
     """Use this data model to parse the request body JSON."""
     title: str = Field(...,
                        example='This is a Random Title')
@@ -39,9 +39,21 @@ class Item(BaseModel):
                              example="Made this meme for the upcoming tsm match. Don't have a good"
                                      " editing software but I hope you enjoy! #C9WIN")
 
+    @validator('title')
+    def check_title(cls, value):
+        """Validate that title is a string."""
+        assert type(value) == str, f'Title == {value}, must be a string'
+        return value
+
+    @validator('reddit_post')
+    def check_body(cls, value):
+        """Validate that title is a string."""
+        assert type(value) == str, f'Body == {value}, must be a string'
+        return value
+
 
 @router.post('/predict')
-async def predict(item: Item):
+async def predict(item: Subreddit_Post):
     """
     Make predictions for classification problem 🔮
 
@@ -51,17 +63,18 @@ async def predict(item: Item):
 
     ### Response
     - `prediction`: predicted subreddit category
-    - `predict_proba`: float between 0.5 and 1.0, 
-    representing the predicted class's probability
+    - `predict_proba`: gives the probability for post of interest
 
     Replace the placeholder docstring and fake predictions with your own model.
     """
     post = item.reddit_post
     reddit_embedding = BASILICA.embed_sentence(post, model='reddit')
     y_pred = log_reg3.predict(np.array(reddit_embedding).reshape(1, -1))
-    prediction = str(y_pred[0])
+    y_proba = log_reg3.predict_proba(np.array(reddit_embedding).reshape(1, -1))
+
     return {
         'Reditt_Post': item.title,
-        'Predicted_Subreddit': prediction,
+        'Predicted_Subreddit': str(y_pred[0]),
+        'Probabillity': str(y_proba[0][0]),
 
     }
